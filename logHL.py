39b97718bb86ab82aaec22c5f9155f39cf77ae16
Geoff Rosenberg <geoff.rosenberg@gmail.com>
2023-01-24 22:07:28 -0800

    Track current map, reset score between maps.

--- logHL.py
+++ logHL.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Log UDP messages frm halflife server."""
+"""Log UDP messages from halflife server."""
 import socket
 import re
 import os
 
 players = {}
+currentMap = 'crossfire'
 
 class Player:
     def __init__(self, name: str):
@@ -17,11 +18,10 @@ def GetPlayerNameAndId(dataStr: str):
     """Get player name from the beginning of the log string."""
     expr = re.compile('\"((\\w+\\s*)+)<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>')
     matches = expr.search(dataStr)
-    playerName = ''
+    playerNameAndId = []
     if matches is not None:
         playerName = matches.groups()[1]
         playerId = matches.groups()[2]
-        playerNameAndId = []
         playerNameAndId.append(playerName)
         playerNameAndId.append(playerId)
     return playerNameAndId
@@ -30,23 +30,34 @@ def GetPlayerNameAndId(dataStr: str):
 def UpdateLogFile(fileName: str):
     """Update the log file."""
     with open(fileName, "w") as logfile:
+        logfile.write("Current map: {0}\n".format(currentMap))
         logfile.write("Player\tKills\tDeaths\n")
         for player in players:
             logfile.write("{0}\t{1}\t{2}\n".format(players[player].name, players[player].kills, players[player].deaths))
         logfile.write("\n")
 
 
+def ResetScore():
+    """Reset the score for all players."""
+    for player in players:
+        players[player].kills = 0
+        players[player].deaths = 0
+
+
 def AddPlayer(dataStr: str):
+    """Add a new player."""
     nameAndId = GetPlayerNameAndId(dataStr)
     players[nameAndId[1]] = Player(nameAndId[0])
 
 
 def RemovePlayer(dataStr: str):
+    """Remove a plyer from the map."""
     nameAndId = GetPlayerNameAndId(dataStr)
     players.pop(nameAndId[1])
 
 
 def UpdateScore(dataStr: str):
+    """Update player scores when one player kills another."""
     expr = re.compile('\"((\\w+\\s*)+)<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>.*\"((\\w+\\s*)+)<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>')
     matches = expr.search(dataStr)
     if matches is not None:
@@ -56,13 +67,29 @@ def UpdateScore(dataStr: str):
         players[idKillee].deaths += 1
 
 
+def HandleMapChange(dataStr: str):
+    """Update current map and reset player scores."""
+    mapExpr = re.compile('Started map \\"(w+)\\"')
+    matches = mapExpr.search(dataStr)
+    if matches is not None:
+        currentMap = matches.groups()[1]
+    ResetScore()
+
+
 def HandleSuicide(dataStr: str):
+    """Adjust a player's score after a self-kill."""
     id = GetPlayerNameAndId(dataStr)[1]
-    players[id].kills  -= 1
+    killPenalty = -1
+    worldExpr = re.compile('world')
+    matches = worldExpr.search(dataStr)
+    if matches is not None:
+        killPenalty = 0
+    players[id].kills  += killPenalty
     players[id].deaths += 1
 
 
 def HandleNameChange(dataStr: str):
+    """Update a player's name."""
     id = GetPlayerNameAndId(dataStr)[1]
     nameExpr = re.compile('.*changed name to \"((\\w+\\s*)+)\"')
     matches = nameExpr.search(dataStr)
@@ -70,8 +97,9 @@ def HandleNameChange(dataStr: str):
         newName = matches.groups()[1]
         players[id].name = newName
 
+
 def ProcessLogMessages(data: bytes):
-    """Apply the regular expression."""
+    """Apply the appropriate regular expression to log messages and handle them."""
     dataStr = str(data)
     print(dataStr)
 
@@ -103,6 +131,12 @@ def ProcessLogMessages(data: bytes):
     nameChangeExpr = re.compile('\" changed name to \"')
     if nameChangeExpr.search(dataStr) is not None:
         HandleNameChange(dataStr)
+        return
+
+    mapChangeExpr = re.compile('Started map')
+    if mapChangeExpr.search(dataStr) is not None:
+        HandleMapChange(dataStr)
+        return
 
 
 if __name__ == "__main__":
