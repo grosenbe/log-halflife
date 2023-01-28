15596fd73712bee23f1df6a439837f030b4be992
Geoff Rosenberg <geoff.rosenberg@gmail.com>
2023-01-27 23:57:09 -0800

    Add console printing, use tabulate

--- logHL.py
+++ logHL.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""Log UDP messages from halflife server."""
+"""Log UDP messages from a Half-Life server."""
 import socket
 import re
 import os
+import sys
+from tabulate import tabulate
 
 players = {}
-currentMap = 'crossfire'
+currentMap = 'crossfireXL'
 
 class Player:
     def __init__(self, name: str):
@@ -14,31 +16,45 @@ class Player:
         self.deaths = 0
 
 
-def GetPlayerNameAndId(dataStr: str):
-    """Get player name from the beginning of the log string."""
-    expr = re.compile('\"((\\w+\\s*)+)<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>')
-    matches = expr.search(dataStr)
-    playerNameAndId = []
-    if matches is not None:
-        playerName = matches.groups()[1]
-        playerId = matches.groups()[2]
-        playerNameAndId.append(playerName)
-        playerNameAndId.append(playerId)
-    return playerNameAndId
+def PrintToConsole():
+    global players
+    global currentMap
+    print("Current map: {0}".format(currentMap))
+    output=[]
+    for player in players:
+        output.append([players[player].name, players[player].kills, players[player].deaths])
+    print(tabulate(output, headers=["Player","Kills","Deaths"]))
+    sys.stdout.flush()
 
 
 def UpdateLogFile(fileName: str):
     """Update the log file."""
     with open(fileName, "w") as logfile:
         logfile.write("Current map: {0}\n".format(currentMap))
-        logfile.write("Player\tKills\tDeaths\n")
+        output=[]
+        global players
         for player in players:
-            logfile.write("{0}\t{1}\t{2}\n".format(players[player].name, players[player].kills, players[player].deaths))
+            output.append([players[player].name, players[player].kills, players[player].deaths])
+        logfile.write(tabulate(output, headers=["Player","Kills","Deaths"]))
         logfile.write("\n")
+
+
+def GetPlayerNameAndId(dataStr: str):
+    """Get player name from the beginning of the log string."""
+    expr = re.compile('\"((?:\\w+\\s*)+)<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>')
+    matches = expr.search(dataStr)
+    playerNameAndId = []
+    if matches is not None:
+        playerName = matches.groups()[0]
+        playerId = matches.groups()[1]
+        playerNameAndId.append(playerName)
+        playerNameAndId.append(playerId)
+    return playerNameAndId
 
 
 def ResetScore():
     """Reset the score for all players."""
+    global players
     for player in players:
         players[player].kills = 0
         players[player].deaths = 0
@@ -47,32 +63,36 @@ def ResetScore():
 def AddPlayer(dataStr: str):
     """Add a new player."""
     nameAndId = GetPlayerNameAndId(dataStr)
+    global players
     players[nameAndId[1]] = Player(nameAndId[0])
 
 
 def RemovePlayer(dataStr: str):
     """Remove a plyer from the map."""
     nameAndId = GetPlayerNameAndId(dataStr)
+    global players
     players.pop(nameAndId[1])
 
 
 def UpdateScore(dataStr: str):
     """Update player scores when one player kills another."""
-    expr = re.compile('\"((\\w+\\s*)+)<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>.*\"((\\w+\\s*)+)<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>')
+    expr = re.compile('\"(?:\\w+\\s*)+<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>.*\"(?:\\w+\\s*)+<[0-9]+><STEAM_[0-9]:[0-9]:([0-9]+)>')
     matches = expr.search(dataStr)
     if matches is not None:
-        idKiller = matches.groups()[2]
-        idKillee = matches.groups()[5]
+        idKiller = matches.groups()[0]
+        idKillee = matches.groups()[1]
+        global players
         players[idKiller].kills  += 1
         players[idKillee].deaths += 1
 
 
 def HandleMapChange(dataStr: str):
     """Update current map and reset player scores."""
-    mapExpr = re.compile('Started map \\"(w+)\\"')
+    mapExpr = re.compile('Started map \\"(\\w+)\\"')
     matches = mapExpr.search(dataStr)
     if matches is not None:
-        currentMap = matches.groups()[1]
+        global currentMap
+        currentMap = matches.groups()[0]
     ResetScore()
 
 
@@ -84,6 +104,8 @@ def HandleSuicide(dataStr: str):
     matches = worldExpr.search(dataStr)
     if matches is not None:
         killPenalty = 0
+
+    global players
     players[id].kills  += killPenalty
     players[id].deaths += 1
 
@@ -91,10 +113,11 @@ def HandleSuicide(dataStr: str):
 def HandleNameChange(dataStr: str):
     """Update a player's name."""
     id = GetPlayerNameAndId(dataStr)[1]
-    nameExpr = re.compile('.*changed name to \"((\\w+\\s*)+)\"')
+    nameExpr = re.compile('.*changed name to \"((?:\\w+\\s*)+)\"')
     matches = nameExpr.search(dataStr)
     if matches is not None:
-        newName = matches.groups()[1]
+        newName = matches.groups()[0]
+        global players
         players[id].name = newName
 
 
@@ -102,43 +125,47 @@ def ProcessLogMessages(data: bytes):
     """Apply the appropriate regular expression to log messages and handle them."""
     dataStr = str(data)
     logFileName = "/home/geoffrosenberg/Documents/connections.txt"
+    processedMessage = False
 
     connectedExpr = re.compile('\\bconnected')
     if connectedExpr.search(dataStr) is not None:
         AddPlayer(dataStr)
         UpdateLogFile(logFileName)
-        return
+        processedMessage = True
 
     disconnectedExpr = re.compile('\\bdisconnected')
     if disconnectedExpr.search(dataStr) is not None:
         RemovePlayer(dataStr)
+        global players
         if players == {} and os.path.exists(logFileName):
             os.remove(logFileName)
-        return
+        processedMessage = True
 
     killedExpr = re.compile('\\bkilled')
     if killedExpr.search(dataStr) is not None:
         UpdateScore(dataStr)
-        return
+        processedMessage = True
 
     suicideExpr = re.compile('\\bsuicide')
     if suicideExpr.search(dataStr) is not None:
         HandleSuicide(dataStr)
-        return
+        processedMessage = True
 
     nameChangeExpr = re.compile('\" changed name to \"')
     if nameChangeExpr.search(dataStr) is not None:
         HandleNameChange(dataStr)
-        return
+        processedMessage = True
 
     mapChangeExpr = re.compile('Started map')
     if mapChangeExpr.search(dataStr) is not None:
         HandleMapChange(dataStr)
-        return
+        processedMessage = True
+
+    if processedMessage == True:
+        PrintToConsole()
 
 
 if __name__ == "__main__":
-    hostname = socket.gethostname()
     UDP_IP = "127.0.0.1"
     UDP_PORT = 11001
 
